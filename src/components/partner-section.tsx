754d98acd265af3c9294be0cb644a3420f80e905
Sajal Kaundal <kaundal.sajal@rwit.io>
2026-07-23 18:12:33 +0530

    feat: update partner section with new images and adjust layout

--- src/components/partner-section.tsx
+++ src/components/partner-section.tsx
@@ -3,16 +3,18 @@ import Image from "next/image";
 
 function PartnerSection() {
   return (
-    <section className="flex max-w-292.5 w-full overflow-hidden gap-25 items-center justify-center animate-marquee px-10 sm:px-8 mt-10 mx-auto">
-      {partnerSectionImages.map((image, index) => (
-        <Image
-          key={index}
-          src={image.src}
-          alt={image.alt}
-          height={image.height}
-          width={image.width}
-        />
-      ))}
+    <section className="flex  w-full overflow-hidden px-10 sm:px-8 mt-10 mx-auto">
+      <div className="flex animate-marquee gap-25 items-center justify-center">
+        {partnerSectionImages.map((image, index) => (
+          <Image
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            height={image.height}
+            width={image.width}
+          />
+        ))}
+      </div>
     </section>
   );
 }
